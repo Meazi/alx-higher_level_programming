731b8d0abe9cb2f5bbf5dffe106a921ccde6ce1b
Meazi <meazimekonnen93@gmail.com>
2022-12-13 16:35:34 +0300

    print names in alphabetical order

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-    for name in dir(hidden_4):
-        if name.startswith('__'):
+    for item in dir(hidden_4):
+        if "__" in item:
             continue
-        print("{:s}".format(name))
+        print("{:s}".format(item))
