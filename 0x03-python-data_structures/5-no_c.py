56f1a27bc6e46e8cbb34cee3aadd5050d33e8254
Meazi <meazimekonnen93@gmail.com>
2022-12-21 17:02:00 +0300

    remove character from a string

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -5,6 +5,5 @@ def no_c(my_string):
         if ord(ch) == 67 or ord(ch) == 99:
             continue
         else:
-            # print(ch) 
             new_string = new_string + ch
-    return(new_string)
+    return (new_string)
