df4c1d24a8f0d96928faf281b835faa8c7815422
Meazi <meazimekonnen93@gmail.com>
2022-12-21 15:23:38 +0300

    edge case added

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -5,7 +5,9 @@ def new_in_list(my_list, idx, element):
     lst = []
     for ls in my_list:
         lst.append(ls)
-    if idx <= len(lst) - 1 and idx >= 0:
+    if idx >= len(lst) - 1 and idx <= 0:
+        return(my_list)
+    else:
         lst[idx] = element
         new_element = lst
         return(lst)
