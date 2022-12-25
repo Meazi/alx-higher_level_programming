039b7562e2ca7a68af8b4f6e9801aea2a4527b69
Meazi <meazimekonnen93@gmail.com>
2022-12-25 16:46:23 +0300

    find the largest number

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    largest_num = None
+    if my_list == None:
+        return ("None")
+    else:
+        for num in my_list:
+            if largest_num is None or largest_num < num:
+                largest_num = num
+        return (largest_num)
