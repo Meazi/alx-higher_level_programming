4f386d9bfc85f4949465b90df5731fa56ed451a8
Meazi <meazimekonnen93@gmail.com>
2022-12-25 16:11:30 +0300

    print length of a sentence and first letter

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     for itm in sentence:
-        return (len(sentence), itm[0])
+        if len(sentence) == 0:
+            return (0, None)
+        else:
+            return (len(sentence), itm[0])
